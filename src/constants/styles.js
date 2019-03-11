55387e3d9b93d4b7fd202589fd72e32a4b8d1d79
George <g7chu@Temps-MBP.hawaii.rr.com>
2019-03-10 17:15:27 -1000

    created constant colors and downloaded images

---
+++ src/constants/styles.js
@@ -0,0 +1,11 @@
+export const COLORS = {
+  GREY: {
+    LIGHT_GREY: '#e0e0e0'
+  },
+  ORANGE: {
+    ORANGE: '#ff7900'
+  },
+  WHITE: {
+    WHITE: '#f5f5f5'
+  }
+}
---
+++ src/data/data.js
@@ -0,0 +1,45 @@
+const FAIRY_TAIL = require('../../assets/images/fairy-tail.jpg');
+const HUNTER_X_HUNTER = require('../../assets/images/hunter-x-hunter.jpg');
+const SHEILD_HERO = require('../../assets/images/sheild-hero.jpg');
+const SLIME = require('../../assets/images/slime.jpg');
+const SWORD_ART_ONLINE = require('../../assets/images/sword-art-online.jpg');
+
+const SHOWS = [
+  {
+    title: 'Fairy Tail',
+    summary: 'The story follows a teenage girl named Lucy Heartfilla who is determined to join the notorious magical Fairy Tail Guild. During a daring rescue, she encounters Natsu who is part of the guild and eventually offers her a place. They become teammates performing various missions for the Fairy Tail Guild. Be prepared for an action-packed adventure!',
+    starring: 'Lucy, Natsu, Gray',
+    publisher: 'TV Tokyo Fairy Tail',
+    image: FAIRY_TAIL
+  },
+  {
+    title: 'Hunter X Hunter',
+    summary: 'Gon, a young boy who lives on Whale Island, dreams of becoming a Hunter like his father, who left when Gon was still young.',
+    starring: 'Gon Freecss, Kilua Zoldyck',
+    publisher: 'NTV',
+    image: HUNTER_X_HUNTER
+  },
+  {
+    title: 'The Rising of the Shield Hero',
+    summary: 'Watani Naofumi, a run-of-the-mill otaku, finds a book in the library that summons him to another world. He is tasked with joining the sword, spear, and bow as one of the Four Cardinal Heroes and fighting the Waves of Catastrophe as the Shield Hero. Excited by the prospect of a grand adventure, Naofumi sets off with his party. ',
+    starring: 'Naofumi, Raphtalia, Filo',
+    publisher: 'Kadokawa Pictures Inc.',
+    image: SHEILD_HERO
+  },
+  {
+    title: 'That Time I Got Reincarnated as a Slime',
+    summary: 'Corporate worker Mikami Satoru is stabbed by a random killer, and is reborn to an alternate world. But he turns out to be reborn a slime! Thrown into this new world with the name Rimuru, he begins his quest to create a world that’s welcoming to all races.',
+    starring: 'Rimuru, Benimaru, Gobuta',
+    publisher: 'Kodansha Ltd.',
+    image: SLIME
+  },
+  {
+    title: 'Sword Art Online',
+    summary: 'n the near future, a Virtual Reality Massive Multiplayer Online Role-Playing Game (VRMMORPG) called Sword Art Online has been released where players control their avatars with their bodies using a piece of technology called: Nerve Gear. One day, players discover they cannot log out, as the game creator is holding them captive unless they reach the 100th floor of the game tower and defeat the final boss.',
+    starring: 'Kirito, Asuna',
+    publisher: 'Aniplex of America',
+    image: SWORD_ART_ONLINE
+  },
+]
+
+export default SHOWS;
