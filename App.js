9b4797ebcaa7fc650574be80ba699d0d2507a75d
George <g7chu@Temps-MBP.hawaii.rr.com>
2019-03-20 09:19:19 -1000

    implemeneted navigation in App, but still needs to stand alone

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, StatusBar } from 'react-native';
+import { StyleSheet, View, StatusBar, Text, Button } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 import HomeScreen from './src/components/Screens/HomeScreen';
+import ShowDetailsScreen from './src/components/Screens/ShowDetailsScreen';
 
 class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <StatusBar hidden={true} />
-        <HomeScreen />
+         <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('ShowDetails')}
+        />
      </View>
     );
   }
@@ -21,4 +26,11 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+const AppNavigator = createStackNavigator(
+  {
+    Home: {screen: HomeScreen},
+    ShowDetails: {screen: ShowDetailsScreen}
+  },
+);
+
+export default createAppContainer(AppNavigator);
